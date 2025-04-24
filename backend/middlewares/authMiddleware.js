154d29fcd4edd559421741b2bf3e569a446f338e
Anmollw <anmolwadhwa2006@outlook.com>
2025-04-24 16:31:42 +0530

    Added admin only middleware

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -23,3 +23,17 @@ const protect = async (req,res,next)=>{
 
     }
 }
+
+
+
+const adminOnly = async (req,res,next)=>{
+    if ( req.user && req.user.role === "admin"){
+        next();
+    } else {
+        res.status(403).json({
+            message : "Access denied , admin only"
+        })
+    }
+}
+
+module.exports = { protect , adminOnly};
