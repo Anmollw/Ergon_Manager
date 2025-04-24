4d41bfd88b85f9b28b17514fa06393cb49ea3dec
Anmollw <anmolwadhwa2006@outlook.com>
2025-04-24 19:37:55 +0530

    Imported middleware , route functions

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const { registerUser, loginUser, getUserProfile, updateUserProfile } = require('../controllers/authController');
+const { protect } = require('../middlewares/authMiddleware');
 
 const authRouter = express.Router();
 
 authRouter.post('/register', registerUser);
-authRouter.post('login', loginUser);
+authRouter.post('/login', loginUser);
 authRouter.get("/profile", protect , getUserProfile);
 authRouter.put("/profile", protect , updateUserProfile);
 
