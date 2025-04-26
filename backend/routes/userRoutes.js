ea8fbc71865083ac0a54276f944445f1bd8ac845
Anmollw <anmolwadhwa2006@outlook.com>
2025-04-26 19:04:24 +0530

    Removed user deletion route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,6 +5,5 @@ const userRouter = express.Router();
 
 userRouter.get('/', protect , adminOnly , getUsers);            // get all users(admin access required)
 userRouter.get('/:id', protect , getUserById);                 //specific user
-userRouter.delete('/:id', protect , adminOnly , deleteUser);   // deleting a user(admin permis.)
 
 module.exports = userRouter;
