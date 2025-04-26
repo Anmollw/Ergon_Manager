1111375164f48a36fcc246f9bd2df6001a406ff7
Anmollw <anmolwadhwa2006@outlook.com>
2025-04-26 19:14:30 +0530

    Added all task management routes

---
+++ backend/routes/taskRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { protect , adminOnly } = require("../middlewares/authMiddleware");
+const taskRouter = express.Router();
+
+// task management
+
+taskRouter.get("dashboard-data", protect, getDashboardData); 
+taskRouter.get('user-dashboard-data', protect, getUserDashboardData);
+taskRouter.get("/", protect, getTasks);  //get all tasks(admin - all , user - assigned)
+taskRouter.get('/:id', protect, getTaskById); // 
+taskRouter.post('/', protect, adminOnly, createTask); //create task - admin
+taskRouter.put('/:id', protect, updateTask);
+taskRouter.delete("/:id", protect , adminOnly , deleteTask); // create task - admin
+taskRouter.put("/:id/status", protect , updateTaskStatus);
+taskRouter.put('/:id/todo', protect , updateTaskChecklist);
+
+module.exports = taskRouter;
