b2eca4e4a0c0f62a32e5e288cb4e10958ccf2d9b
Anmollw <anmolwadhwa2006@outlook.com>
2025-04-28 20:57:58 +0530

    Added create task handler

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -13,31 +13,125 @@ const getTasks= async(req,res)=>{
 
 
 //route : GET /api/v1/tasks/:id
-const getTaskById = async(req,res)=>{};
+const getTaskById = async(req,res)=>{
+    try{
+
+    } catch(error){
+        res.status(500).json({
+            message : "Server error" , error : error.message
+        });
+    }
+};
 
 
 //route : POST /api/v1/tasks/  (admin)
-const createTask = async(req,res)=>{};
+const createTask = async(req,res)=>{
+    try{
+        const {
+            title,
+            description,
+            priority,
+            dueDate,
+            assignedTo,
+            attachments,
+            todoChecklist,
+        } = req.body;
+
+        if(!Array.isArray(assignedTo)){
+            return res.status(500).json({
+                message : "assignedTo must be an array of user IDs"
+            });
+        }
+
+        const task = await Task.create({
+            title,
+            description,
+            priority,
+            dueDate,
+            assignedTo,
+            createdBy: req.user._id,
+            todoChecklist,
+            attachments
+        });
+
+        res.status(201).json({
+            message : "Task created succesfully"  , task 
+        });
+
+    } catch(error){
+        res.status(500).json({
+            message : "Server error" , error : error.message
+        });
+    }
+};
 
 
 // route : PUT /api/v1/tasks/:id
-const updateTask = async(req,res)=>{};
+const updateTask = async(req,res)=>{
+    try{
+
+    } catch(error){
+        res.status(500).json({
+            message : "Server error" , error : error.message
+        });
+    }
+};
 
 //route : DELETE /api/v1/tasks/:id (admin)
-const deleteTask = async(req,res)=>{};
+const deleteTask = async(req,res)=>{
+    try{
+
+    } catch(error){
+        res.status(500).json({
+            message : "Server error" , error : error.message
+        });
+    }
+};
 
 
 //route : PUT /api/v1/tasks/:id/status
-const updateTaskStatus = async(req,res)=>{};
+const updateTaskStatus = async(req,res)=>{
+    try{
+
+    } catch(error){
+        res.status(500).json({
+            message : "Server error" , error : error.message
+        });
+    }
+};
 
 //route : PUT /api/v1/tasks/:id/todo
-const updateTaskChecklist = async(req,res)=>{};
+const updateTaskChecklist = async(req,res)=>{
+    try{
+
+    } catch(error){
+        res.status(500).json({
+            message : "Server error" , error : error.message
+        });
+    }
+};
 
 //route : GET /api/v1/tasks/dashboard-data
-const getDashboardData = async(req,res)=>{};
+const getDashboardData = async(req,res)=>{
+    try{
+
+    } catch(error){
+        res.status(500).json({
+            message : "Server error" , error : error.message
+        });
+    }
+};
 
 //route : GET /api/v1/tasks/user-dashboard-data
-const getUserDashboardData = async(req,res)=>{};
+const getUserDashboardData = async(req,res)=>{
+    try{
+
+    } catch(error){
+        res.status(500).json({
+            message : "Server error" , error : error.message
+        });
+    }
+};
 
 module.exports={
     getTasks,
