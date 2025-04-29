94ae1079e38510982e6594225e75d9ff3744cb0d
Anmollw <anmolwadhwa2006@outlook.com>
2025-04-29 23:46:12 +0530

    Added Update Task handler

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -78,6 +78,18 @@ const getTasks= async(req,res)=>{
 //route : GET /api/v1/tasks/:id
 const getTaskById = async(req,res)=>{
     try{
+        const task = await Task.findById(req.params.id).populate(
+            "assignedTo" ,
+            "name email profileImageUrl"
+        );
+
+        if(!task) {
+            return res.status(404).json({
+                message : "Task not found"
+            })
+        }
+
+        res.json(task);
 
     } catch(error){
         res.status(500).json({
@@ -132,6 +144,41 @@ const createTask = async(req,res)=>{
 // route : PUT /api/v1/tasks/:id
 const updateTask = async(req,res)=>{
     try{
+        const task = await Task.findById(req.params.id)
+
+        if(!task) {
+            return res.status(404).json({
+                message : "Task not found"
+            })
+        }
+
+        task.title = req.body.title || task.title;
+        task.description = req.body.description || task.description;
+        task.priority = req.body.priority || task.priority;
+        task.dueDate = req.body.dueDate || task.dueDate;
+        task.todoChecklist = req.body.todoChecklist || task.todoChecklist;
+        task.attachments = req.body.attachments || task.attachments;
+
+        if(req.body.assignedTo){
+            if(!Array.isArray(req.body.assignedTo)){
+                return res.status(404).json({
+                    message : "assignedTo must be an array of user IDs"
+                });
+            }
+
+            task.assignedTo = req.body.assignedTo;
+        }
+
+        const updatedTask = await task.save();
+        res.json({
+            message : "Task updated successfully" ,
+            updatedTask
+        });
+
+        
+
+
+
 
     } catch(error){
         res.status(500).json({
