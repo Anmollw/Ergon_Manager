76cd50682af57aeee9e3cc85dfdb653266865843
Anmollw <anmolwadhwa2006@outlook.com>
2025-05-01 15:26:55 +0530

    Added delete task handler

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -175,11 +175,6 @@ const updateTask = async(req,res)=>{
             updatedTask
         });
 
-        
-
-
-
-
     } catch(error){
         res.status(500).json({
             message : "Server error" , error : error.message
@@ -190,6 +185,18 @@ const updateTask = async(req,res)=>{
 //route : DELETE /api/v1/tasks/:id (admin)
 const deleteTask = async(req,res)=>{
     try{
+        const task = await Task.findById(req.params.id);
+
+        if(!task) {
+            return res.status(404).json({
+                message : "Task not found"
+            });
+        }
+
+        await task.deleteOne();
+        res.json({
+            message : "Task deleted successfully"
+        });
 
     } catch(error){
         res.status(500).json({
