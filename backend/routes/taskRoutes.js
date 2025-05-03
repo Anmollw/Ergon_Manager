bdae1111c2c80ae7b79ccefaf3a90dd180dcaeb1
Anmollw <anmolwadhwa2006@outlook.com>
2025-05-03 20:41:09 +0530

    Resolved ERROR:  Cast to ObjectId failed

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -5,13 +5,13 @@ const taskRouter = express.Router();
 
 // task management
 
-taskRouter.get("dashboard-data", protect, getDashboardData); 
-taskRouter.get('user-dashboard-data', protect, getUserDashboardData);
+taskRouter.get("/dashboard-data", protect, getDashboardData); 
+taskRouter.get('/user-dashboard-data', protect, getUserDashboardData);
 taskRouter.get("/", protect, getTasks);  //get all tasks(admin - all , user - assigned)
 taskRouter.get('/:id', protect, getTaskById); // 
 taskRouter.post('/', protect, adminOnly, createTask); //create task - admin
 taskRouter.put('/:id', protect, updateTask);
-taskRouter.delete("/:id", protect , adminOnly , deleteTask); // create task - admin
+taskRouter.delete("/:id", protect , adminOnly , deleteTask); // delete task - admin
 taskRouter.put("/:id/status", protect , updateTaskStatus);
 taskRouter.put('/:id/todo', protect , updateTaskChecklist);
 
