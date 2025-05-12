22ce1030c88ec67be75e6b45a3b9a630928bc4b6
Anmollw <anmolwadhwa2006@outlook.com>
2025-05-13 02:10:41 +0530

    Added user report export handler

--- backend/controllers/reportController.js
+++ backend/controllers/reportController.js
@@ -36,13 +36,13 @@ const exportTasksReport = async(req,res) => {
             });
         });
 
-        res.setHeader(                                             // telling client (new learning)
+        res.setHeader(                                             // telling client (new info #3)
             "Content-Type",
             "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
         );
 
         res.setHeader(
-            "Content-Disposition",                                   //handling response(new learning)
+            "Content-Disposition",                                   //handling response(new info#2)
             ' attachment; filename="tasks_report.xlsx" '
         );
 
@@ -63,6 +63,71 @@ const exportTasksReport = async(req,res) => {
 const exportUserReport = async(req,res)=>{
     try {
 
+        const users = await User.find().select("name email _id").lean();           //(new info #3) for devs : used this since the user will go into the excel sheet
+        const userTasks = await Task.find().populate(
+            "assignedTo",
+            "name email _id"
+        );
+
+        const userTaskMap = {};
+        users.forEach((user)=>{
+            userTaskMap[user._id] = {
+                name : user.name,
+                email : user.email,
+                taskCount : 0,
+                pendingTasks : 0,
+                inProgressTasks : 0,
+                completedTasks : 0,
+            };
+        });
+
+        userTasks.forEach((task)=>{
+            if(task.assignedTo){
+                task.assignedTo.forEach((assignedUser)=>{
+                    if(userTaskMap[assignedUser._id]){
+                        userTaskMap[assignedUser._id].taskCount += 1;
+                        if(task.status === "Pending"){
+                            userTaskMap[assignedUser._id].pendingTasks+= 1;    
+                        } else if (task.status === "In Progress"){
+                            userTaskMap[assignedUser._id].inProgressTasks += 1;
+                        } else if(task.status === "Completed"){
+                            userTaskMap[assignedUser._id].completedTasks += 1;
+                        }
+                    }
+                });
+            }
+        });
+
+        const workbook = new excelJS.Workbook();
+        const worksheet = workbook.addWorksheet('User Task Report');
+
+        worksheet.columns = [
+            { header : "User Name" , key : "name" , width : 30 },
+            { header : "Email" , key : "email"  , width : 40 },
+            { header : "Total Assigned Tasks" , key : "taskCount", width : 20 },
+            { header: "Pending Tasks" , key : "pendingTasks" , width : 20 },
+            { header : "In Progress Tasks" , key : "inProgressTasks" , width : 20 },
+            { header : "Completed Tasks"  , key : "completedTasks" , width : 20 } 
+        ];
+        
+        Object.values(userTaskMap).forEach((user)=>{
+            worksheet.addRow(user);
+        })
+
+        res.setHeader(                                             
+            "Content-Type",
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        );
+
+        res.setHeader(
+            "Content-Disposition",                                    
+            ' attachment; filename="user_report.xlsx" '
+        );
+
+        return workbook.xlsx.write(res).then(()=>{
+            res.end();
+        })
+        
     } 
     catch(error){
         res.status(500).json({
