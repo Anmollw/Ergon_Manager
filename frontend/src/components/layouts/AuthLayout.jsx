4ace0d025fb0620b292036705fa1270848a49045
Anmollw <anmolwadhwa2006@outlook.com>
2025-05-15 16:15:02 +0530

    Added Auth Layout Component

--- frontend/src/components/layouts/AuthLayout.jsx
+++ frontend/src/components/layouts/AuthLayout.jsx
@@ -4,8 +4,8 @@ import UI_IMG from "../../assets/images/auth-img2.png"
 
 const AuthLayout = ({ children })=>{
     return <div className="flex">
-        <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
-            <h2 className="text-lg font-medium text-black">Task Manager</h2>
+        <div className="w-screen h-screen md:w-[60vw]  px-12 pt-8 pb-12">
+            <h2 className="text-lg font-medium text-black ml-[39px]">Task Manager</h2>
             {children}
             </div>
 
