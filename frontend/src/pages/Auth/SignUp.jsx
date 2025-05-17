f254aa22a84bf01262c9305dcccbcb88a0513a1f
Anmollw <anmolwadhwa2006@outlook.com>
2025-05-17 12:07:31 +0530

    Added SignUp page logic

--- frontend/src/pages/Auth/SignUp.jsx
+++ frontend/src/pages/Auth/SignUp.jsx
@@ -1,10 +1,53 @@
-import React from "react"
+import React, { use, useState } from "react"
+import AuthLayout from "../../components/layouts/AuthLayout"
+import { validateEmail } from "../../utils/helper";
+import ProfilePhotoSelector from "../../components/Inputs/ProfilePhotoSelector";
 
 const SignUp=()=>{
+    const [profilePic, setProfilePic] = useState(null);
+    const [fullName , setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password , setPassword] = useState("");
+    const [adminInviteToken, setAdminInviteToken] = useState("");
+
+    const [error,setError] = useState(null);
+
+    const handleSignup = async(e)=>{
+        e.preventDefault();
+
+
+        if(!fullName){
+            setError("Please enter full name")
+            return;
+        }
+    
+        if(!validateEmail(email)){
+            setError("Please enter a valid email address.")
+            return;
+        }
+    
+        if(!password){
+            setError("Please enter the password");
+            return;
+        }
+    
+        setError("");
+    
+    };
+
     return( 
-    <div>
-        SignUp
-    </div>
+    <AuthLayout>
+        <div className="lg:w-[to-100% h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center ml-[39px]">
+            <h3 className="text-xl font-semibold text-black">Create an Account</h3>
+            <p className="text-xs text-slate-700 mt-[5px] mb-6">Join us today by entering your details below</p>
+
+            <form onSubmit={handleSignup}>
+                <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4"></div>
+            </form>
+        </div>
+        
+    </AuthLayout>
     )
 }
 
