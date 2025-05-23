e574bb178f5105f756bda53c92dbe17252d6fb44
Anmollw <anmolwadhwa2006@outlook.com>
2025-05-23 18:48:06 +0530

    Added Tailwind to divs

--- frontend/src/components/Inputs/ProfilePhotoSelector.jsx
+++ frontend/src/components/Inputs/ProfilePhotoSelector.jsx
@@ -19,9 +19,36 @@ const ProfilePhotoSelector = ({image,setImage})=>{
         setImage(null);
         setPreviewUrl(null);
     };
+
+    const onChooseFile = ()=>{
+        inputRef.current.click();
+    }
     return (
-        <div>
-            pfp Selector
+        <div className="flex justify-center mb-6">
+            <input 
+            type="file"
+            accept="image/*"
+            ref={inputRef}
+            onChange={handleImageChange}
+            className="hidden"
+            />
+
+            {!image?(
+                <div className="w-20 h-20 flex items-center justify-center bg-blue-100/50 rounded-full relative cursor-pointer">
+                    <LuUser className="text-4xl text-primary" />
+
+                    <button type="button" className="w-8 h-8 flex items-center justify-center bg-primary text-white rounded-full absolute -bottom-1 -right-1 cursor-pointer" onClick={onChooseFile}>
+                        <LuUpload />
+                    </button>
+               </div>
+            ) : (
+                <div className="relative">
+                    <img src={previewUrl} alt="profile photo" className="w-20 h-20 rounded-full object-cover" />
+                    <button type="button" className="w-8 h-8 flex items-center justify-center bg-red-500 text-white rounded-full absolute -bottom-1 -right-1" onClick={handleRemoveImage}>
+                        <LuTrash />
+                    </button>
+                </div>
+            )}
         </div>
     )
 }
