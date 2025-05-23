818a709bad423a8366e7011dfee77ae9a6e3e8f5
Anmollw <anmolwadhwa2006@outlook.com>
2025-05-23 19:11:39 +0530

    Added input fields and link to login route in Signup page

--- frontend/src/pages/Auth/SignUp.jsx
+++ frontend/src/pages/Auth/SignUp.jsx
@@ -2,6 +2,8 @@ import React, { use, useState } from "react"
 import AuthLayout from "../../components/layouts/AuthLayout"
 import { validateEmail } from "../../utils/helper";
 import ProfilePhotoSelector from "../../components/Inputs/ProfilePhotoSelector";
+import Input from "../../components/Inputs/Input";
+import { Link } from "react-router-dom";
 
 const SignUp=()=>{
     const [profilePic, setProfilePic] = useState(null);
@@ -43,10 +45,54 @@ const SignUp=()=>{
 
             <form onSubmit={handleSignup}>
                 <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-4"></div>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                    <Input
+                      value = {fullName}
+                      onChange={({target}) => setFullName(target.value)}
+                      label={"Full name"}
+                      placeholder="Jacky"
+                      type="text"
+                    />
+
+                    <Input 
+                     value={email} 
+                     onChange={({target})=> setEmail(target.value)}
+                     label = "Email Address"
+                     placeholder = "jacob@example.com" 
+                     type = "text"
+                    />
+
+                    <Input 
+                     value={password} 
+                     onChange={({target})=> setPassword(target.value)}
+                     label = "Password"
+                     placeholder = "Min 8 Characters" 
+                     type = "password"
+                    /> 
+
+                    <Input 
+                     value={adminInviteToken} 
+                     onChange={({target})=> setAdminInviteToken(target.value)}
+                     label = "Admin Invite Token"
+                     placeholder = "6 Digit Code" 
+                     type = "text"
+                    />
+                </div>
+
+                    {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>} 
+
+                    <button type="submit" className="btn-primary">
+                        SIGN UP
+                    </button>
+
+                    <p className="text-[13px] text-slate-800 mt-3">
+                        Already have an account?{" "}
+                        <Link className="font-medium text-primary underline" to="/login">
+                            Login
+                        </Link>
+                    </p>
             </form>
         </div>
-        
     </AuthLayout>
     )
 }
