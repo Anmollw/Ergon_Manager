0005e01fac4c341840688d36a4aadde1518b5326
Anmollw <anmolwadhwa2006@outlook.com>
2025-05-24 13:58:09 +0530

    Added axios instance response interceptor

--- frontend/src/utils/axiosinstance.js
+++ frontend/src/utils/axiosinstance.js
@@ -22,3 +22,23 @@ axiosInstance.interceptors.request.use(
         return Promise.reject(error);
     }
 );
+
+axiosInstance.interceptors.response.use(
+    (response)=>{
+        return response;
+    },
+    (error)=>{
+        if(error.response){
+            if(error.response.status === 401){
+                window.location.href = "/login";
+            } else if(error.response.status === 500){
+                console.error("Server error. Please try again later.");
+            }
+        } else if (error.code === "ECONNABORTED"){
+            console.error("Request timeout. Please try again later.");
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default axiosInstance;
